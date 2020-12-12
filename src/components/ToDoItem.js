2c901e9c89d3f1545bf65b89a200d98db719ee9f
Tazwar Islam <5572533-tazwar9t6@users.noreply.gitlab.com>
2020-12-12 09:58:10 +0600

    added done function

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { deleteToDo } from '../redux/actions/ToDoActions';
 
 const ToDoItem = ({ToDo}) => {
     let {id , name} = ToDo;
-    console.log(ToDo);
+    // console.log(ToDo);
+    let dispatch = useDispatch();
     return (
         <div class="col-md-6 mx-auto text-center mt-5">
             <div  className="row mt-3 justify-content-between align-items-center">
@@ -17,7 +20,9 @@ const ToDoItem = ({ToDo}) => {
                 </div>
                 <div>
                 <Button className="mx-1" variant="outline-warning">Edit</Button>
-                <Button className="mx-1"variant="outline-success">Done</Button>
+                <Button className="mx-1"variant="outline-success" 
+                    onClick={()=> dispatch(deleteToDo(id))}
+                >Done</Button>
                 </div>
             </div>
         </div>
--- src/redux/Reducers/ToDoReducers.js
+++ src/redux/Reducers/ToDoReducers.js
@@ -2,14 +2,16 @@ import { ADD_TO_DO, DELETE_TO_DO, UPDATE_TO_DO } from "../actions/ToDoActions";
 import { ToDos } from "../states/ToDoStates";
 
 export let ToDoReducers = (state=ToDos, action) =>{
+    
     switch (action.type){
         case ADD_TO_DO:
             break;
         case DELETE_TO_DO:
-            break;
+            let newToDos = [...state];
+            newToDos = newToDos.filter(toDo => toDo.id != action.payload);
+            return newToDos;
         case UPDATE_TO_DO:
             break;
-        default:
-            return state;
     }
+    return state;
 }
